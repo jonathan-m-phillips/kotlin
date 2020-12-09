1c6a914ff311606b268b594a53d39f6ec6e49f5c
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-12-09 15:57:04 -0600

    learning data types in kotlin

--- app/src/main/java/com/example/helloworld/KotlinBasics.kt
+++ app/src/main/java/com/example/helloworld/KotlinBasics.kt
@@ -9,11 +9,56 @@ fun main() {
     // var myName = "Jon"
     // myName = "Bob"
 
+    // immutable variable -- constant
+    // string type
     val myName = "Jon"
 
     // string concatenation
-    print("Hello " + myName)
+    println("Hello " + myName)
 
     // or I can use template
-    print("Hello $myName")
+    println("Hello $myName")
+
+    // type int 32 bit
+    val myAge = 30
+
+    // Integer TYPES - Byte: 8 bit; Short: 16 bit; Int: 32 bit; Long: 64 bit;
+    // variable (name) : (TYPE) = (value)
+    // this is not necessary but can be used
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 1_039_812_309_487_120_394
+
+    // Floating Point number TYPES - Float: 32 bit; Double 64 bit;
+    // Float needs an F at the end of the number or else Kotlin will assume it is a double
+    // Float is for faster calculations -- Double is for exact calculations
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.14159265358979323846
+
+    // Booleans
+    // var isSunny: Boolean = true
+    // or
+    var isSunny = true
+    isSunny = false
+
+    // Characters
+    val letterChar = 'A'
+    val digitChar = '1'
+
+    // Strings
+    val myStr = "Hello World"
+    val firstCharInString = myStr[0]
+    val lastCharInString = myStr[myStr.length - 1]
+
+    // string concatenation
+    println("First character: " + firstCharInString)
+    // template
+    println("First character: $firstCharInString")
+
+    // string concatenation
+    println("Last character: " + lastCharInString)
+    // template
+    println("Last character: $lastCharInString")
+
 }
