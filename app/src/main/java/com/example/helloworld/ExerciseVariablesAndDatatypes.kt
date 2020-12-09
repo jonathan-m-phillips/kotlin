a1020f1e9e6fa0530941189c8e6bf8237f51810c
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-12-09 16:02:01 -0600

    kotlin exercise to use proper datatypes for variables

---
+++ app/src/main/java/com/example/helloworld/ExerciseVariablesAndDatatypes.kt
@@ -0,0 +1,14 @@
+package com.example.helloworld
+
+fun main() {
+
+    val myString: String = "Android Masterclass"
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.14159265358979
+    val myByte: Byte = 25
+    val myShort: Short = 2020
+    val myLong: Long = 18881234567
+    val myBoolean: Boolean = true
+    val myChar: Char = 'A'
+
+}
