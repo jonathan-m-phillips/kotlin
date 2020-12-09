9a725d8385d55589a3219c89ae0827100d3d7ad5
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-12-09 15:00:01 -0600

    cleaned up code, made it more proficient

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,18 +17,18 @@ class MainActivity : AppCompatActivity() {
         // Creating an On Click Listener
 
         // get reference to button
-        val btn_click_me = findViewById(R.id.button) as Button
+        // val btn_click_me = findViewById(R.id.button) as Button
 
         // creating val myTextView and importing TextView class
-        val myTextView = findViewById(R.id.textView) as TextView
+        // val myTextView = findViewById(R.id.textView) as TextView
 
         // creating variable for how many times the button was clicked
         var timesClicked = 0
 
         // set on-click listener
-        btn_click_me.setOnClickListener {
+        button.setOnClickListener {
             timesClicked += 1
-            myTextView.text = timesClicked.toString()
+            textView.text = timesClicked.toString()
             Toast.makeText(this@MainActivity, "Hi Jon.", Toast.LENGTH_SHORT).show()
         }
 
