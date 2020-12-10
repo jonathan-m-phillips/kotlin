bc17bb9ef6acaa89d2652a3f6a6d2b559e8a4476
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-12-09 20:41:27 -0600

    learned lateinit - getters and setters are new to me in kotlin - different than java

---
+++ app/src/main/java/com/example/helloworld/LateinitSettersGetters.kt
@@ -0,0 +1,40 @@
+package com.example.helloworld
+
+fun main() {
+
+    var myCar = Car()
+    println("car brand: ${myCar.myBrand}")
+    myCar.maxSpeed = 200
+    println("max speed: ${myCar.maxSpeed}")
+    println("car model: ${myCar.myModel}")
+
+}
+
+
+class Car() {
+
+    // lateinit means it will be initialized later on
+    lateinit var owner : String
+
+    val myBrand : String = "BMW"
+        // Creating custom getter for myBrand
+    get() {
+        return field.toLowerCase()
+    }
+
+    var maxSpeed : Int = 150
+    // This code is automatically generated for us
+        get() = field
+        set(value){
+            field = if (value > 0) value else throw IllegalArgumentException("Max speed cannot be less than 0")
+        }
+
+    var myModel : String = "M5"
+        private set
+
+    init {
+        this.myModel = "M3"
+        this.owner = "Frank"
+    }
+
+}
