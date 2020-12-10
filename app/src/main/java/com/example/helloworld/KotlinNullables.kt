bbb569fc0194683d9894943e9558d945018c50d0
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-12-09 18:29:59 -0600

    nullable second part - elvis operator

--- app/src/main/java/com/example/helloworld/KotlinNullables.kt
+++ app/src/main/java/com/example/helloworld/KotlinNullables.kt
@@ -2,28 +2,20 @@ package com.example.helloworld
 
 fun main() {
 
-    var name: String = "Jon"
-    // name = null -> Compilation ERROR
 
     var nullableName : String? = "Jon"
-    // nullableName = null
-    // OR
-    // var nullableName : String? = null
 
-    var len = name.length
     var len2 = nullableName?.length
     // println(nullableName?.toLowerCase())
     nullableName?.let { println(it.length) }
 
-    /*
-    if (nullableName != null) {
-        var len2 = nullableName.length
-    } else {
-        null
-    }
-     */
+    // if nullableName is empty --> use default "Guest"
+    // ?: Elvis Operator
+    val name = nullableName ?: "Guest"
+    println("My name is $name")
 
+    println(nullableName!!.toLowerCase())
 
-
+    // val wifeAge: String? = user?.wife?.age ?: 0
 
 }
